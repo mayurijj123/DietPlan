686afe03834b89a6ae1ed9bac433072ccec6950e
YashMahale06 <yashmahale2002@gmail.com>
2023-03-05 00:49:55 +0530

    navbar changed

---
+++ Client/diet-planner/src/components/Nav.js
@@ -0,0 +1,101 @@
+import React from "react";
+import wellness from "../Images/wellness.jpg";
+import { Fragment } from "react";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+
+const navigation = [
+  { name: "Home", href: "/", current: true },
+  { name: "About", href: "/", current: false },
+  { name: "Contact", href: "/", current: false },
+ 
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+const Nav = () => {
+  return (
+    <Disclosure as="nav" className="bg-gradient-to-r from-[#FF7B54]  to-[#FFB26B]">
+      {({ open }) => (
+        <>
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div className="relative flex h-16 items-center justify-between">
+            
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-white hover:bg-green-500 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <a className="flex  ml-20 lg:ml-0 ">
+            <img
+              src={wellness}
+              className="h-6 mr-3 sm:h-9 rounded-full"
+              alt="Flowbite Logo"
+            />
+            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+              Holistic Wellness Clinic
+            </span>
+          </a>
+              <div className="flex flex-1 items-right justify-right sm:items-stretch sm:justify-end">
+                <div className="hidden sm:ml-6 sm:block ">
+
+                  <div className="flex space-x-4 ">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current
+                            ? "bg-green-500 text-white"
+                            : "text-white hover:bg-green-500 hover:text-white",
+                          "px-3 py-2 rounded-md text-sm font-medium"
+                        )}
+                        aria-current={item.current ? "page" : undefined}
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+          
+
+          <Disclosure.Panel className="sm:hidden">
+          
+            <div className="space-y-1 px-2 pt-2 pb-3">
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    item.current
+                      ? "bg-green-500 text-white"
+                      : "text-white hover:bg-green-500 hover:text-white",
+                    "block px-3 py-2 rounded-md text-base font-medium"
+                  )}
+                  aria-current={item.current ? "page" : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+    
+  );
+};
+
+export default Nav;
